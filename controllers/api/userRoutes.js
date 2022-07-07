1bd285a59d9115e76c841f672e8440c664f2b438
rwilliams05 <71559818+rwilliams05@users.noreply.github.com>
2022-07-07 11:14:08 -0400

    add search route, correct sign in

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,14 +3,20 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
-    console.log(userData)
-    let transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_SMTP_ADDRESS,
+    const userData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    console.log(userData);
+
+    //create nodemailer
+   let transporter = nodemailer.createTransport({
+     host: process.env.EMAIL_SMTP_ADDRESS,
       port: process.env.EMAIL_SMTP_PORT,
       secure: process.env.EMAIL_SMTP_PORT === '465',
       auth: {
-        user: process.env.EMAIL_SENDER,
+       user: process.env.EMAIL_SENDER,
         pass: process.env.EMAIL_PASSWORD,
       },
     });
